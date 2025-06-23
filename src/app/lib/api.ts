a7c54212523ed922056f1cf64c9aa7ef62784cd6
jamshed45 <jamshed76@gmail.com>
2025-06-23 18:31:05 +0500

    Update api.ts

--- src/app/lib/api.ts
+++ src/app/lib/api.ts
@@ -5,7 +5,7 @@ if (typeof window !== 'undefined') {
   const origin = window.location.origin;
 
   if (origin === 'http://82.25.105.217:3000') {
-    API_BASE_URL = 'http://82.25.105.217:3000/api'; // Live API
+    API_BASE_URL = 'http://82.25.105.217/api'; // Live API
   } else {
     API_BASE_URL = 'http://localhost:8000/api'; // Local API
   }
