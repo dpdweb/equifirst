5702d4f34b69108ec48b415b51268a065b7dfe7d
jamshed45 <jamshed76@gmail.com>
2025-07-02 23:29:55 +0500

    Update page.tsx

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -7,16 +7,10 @@ import RedefinedYourHome from "../components/RedefinedYourHome";
 
 export default function AboutUs() {
   return (
-    <div className="ef-sub-page-top-style">
-      {/* <MortgageCalculator /> */}
-      <SubPageHeroBanner
-        title="About Us"
-        subtitle="Welcome to Equifirst Capital Financing"
-        image="/assets/images/about-us-hero.png"
-      />
-      <WhoWeAre />
-      <OurTeam />
-      <RedefinedYourHome />
-    </div>
+    <div className="flex items-center justify-center min-h-screen text-center px-4 bg-white dark:bg-black text-black dark:text-white">
+  <h1 className="text-3xl sm:text-5xl font-bold tracking-tight">
+    Coming Soon
+  </h1>
+</div>
   );
 }
