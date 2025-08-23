e115f1509435e556556023d0dc60a895fcfc1d12
jamshed45 <jamshed76@gmail.com>
2025-08-23 18:32:27 +0500

    Update BlogCard.tsx

--- src/app/components/BlogCard.tsx
+++ src/app/components/BlogCard.tsx
@@ -18,7 +18,7 @@ export default function BlogCard({ blog }: { blog: Blog }) {
     <div className=" bg-white rounded-lg overflow-hidden">
       <Image
         src={blog.image || '/assets/images/fallback.jpg'}
-        alt={blog.image}
+        alt={blog.image ?? blog.title}
         width={500}
         height={300}
         className="w-full h-52 object-cover rounded-2xl"
