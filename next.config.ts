dc50f97a000854d9a38a12e68d9192058f5041ef
jamshed45 <jamshed76@gmail.com>
2025-08-23 18:25:23 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,7 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'http',
+        protocol: 'https',
         hostname: 'equifirst.ae',
         port: '', // leave empty unless needed
         pathname: '/**',
