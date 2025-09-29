ba05ba0dedb7213ee4d1cce1a6b103ab9cdda8d0
Fahad <fahad@dpdweb.com>
2025-09-30 01:34:07 +0500

    robot.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,13 +2,11 @@ import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [
-      {
-        userAgent: "*",
-        allow: "/",
-        disallow: ["/zapapi/", "/admin/"],
-      },
-    ],
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: ["/admin", "/zapapi"],
+    },
     sitemap: "https://www.equifirst.ae/sitemap.xml",
   };
 }
