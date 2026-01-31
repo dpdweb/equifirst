b4243af40e8328dc3bb257b8576affacaf7c0936
Fahad <fahad@dpdweb.com>
2026-01-31 23:27:27 +0500

    Fixes

--- src/app/lib/api.tsx
+++ src/app/lib/api.tsx
@@ -1,4 +1,4 @@
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || '';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'https://www.equifirst.ae/api';  // Default to production URL if not set
 
 export { API_BASE_URL };
 
